3d435a0169564290ec01ade901e861a9d3429fa5
vordenken <62395860+vordenken@users.noreply.github.com>
2024-11-26 14:04:49 +0100

    add Sparkle update framework bump version to 0.2

--- AutoPiP/AppDelegate.swift
+++ AutoPiP/AppDelegate.swift
@@ -9,13 +9,34 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    private let updateController = UpdateController()
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
         // Override point for customization after application launch.
+        
+        // Erstelle Update-Menü
+        let mainMenu = NSMenu()
+        let appMenuItem = NSMenuItem()
+        let appMenu = NSMenu()
+        let checkForUpdatesItem = NSMenuItem(
+            title: "Nach Updates suchen...",
+            action: #selector(checkForUpdates(_:)),
+            keyEquivalent: ""
+        )
+        
+        mainMenu.addItem(appMenuItem)
+        appMenuItem.submenu = appMenu
+        appMenu.addItem(checkForUpdatesItem)
+        
+        NSApplication.shared.mainMenu = mainMenu
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
-
+    
+    @objc func checkForUpdates(_ sender: Any?) {
+        updateController.checkForUpdates()
+    }
 }
